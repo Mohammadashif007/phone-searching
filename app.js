1b97e069175008f2c98cc5d0f2e0692434d1f45e
Mohammad Ashif <mohammadashif510@gmail.com>
2023-09-06 20:28:48 +0600

    add done

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-function loadData(phoneName){
+let phoneData = [];
+
+async function loadData(phoneName){
     const url = `https://openapi.programming-hero.com/api/phones?search=${phoneName}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayData(data.data))
+    let data = await fetch(url)
+    data = await data.json()
+    phoneData = data.data
+    displayData(data.data)
 }
 
 function displayData(phones){
@@ -11,6 +14,7 @@ function displayData(phones){
     phones.forEach(phone => {
         console.log(phone)
         const div = document.createElement('div');
+        // const data = JSON.stringify(phone);
         const list = ["card", "w-100", "text-center"];
         div.classList.add(list);
         div.innerHTML = `
@@ -18,7 +22,7 @@ function displayData(phones){
         <div class="card-body text-center">
           <h2 class="text-2xl">${phone.phone_name}</h2>
           <p>${phone.slug}</p>
-          <button class="btn btn-primary">Click</button>
+          <button id="add-to-local" onclick="addToLocal('${phone.slug}')" class="btn btn-primary">Click</button>
         </div>
         `
         phoneContainer.appendChild(div);
@@ -30,5 +34,32 @@ document.getElementById('search-btn').addEventListener('click', () => {
     const inputField = document.getElementById('input-field');
     const inputFieldValue = inputField.value;
     inputField.value = "";
-    loadData(inputFieldValue);
+    
 })
+
+
+const getCard = () => {
+    const storedCard = localStorage.getItem('cart');
+    cart = [];
+    if(storedCard){
+        cart = JSON.parse(storedCard)
+    }
+    return cart
+}
+
+const saveCardToLocalStorage = (product) => {
+    const card = getCard()
+    card.push(product)
+    console.log(card)
+    localStorage.setItem('cart', JSON.stringify(card))
+}
+
+
+function addToLocal(slug) {
+    // console.log(phone)
+    const clickData = phoneData.find(phone => phone.slug == slug)
+    console.log(clickData)
+    saveCardToLocalStorage(clickData)
+}
+
+loadData('oppo');
---
+++ localStorage.js
@@ -0,0 +1,3 @@
+// document.getElementById('add-to-local').addEventListener('click', () => {
+//     console.log('click');
+// })
